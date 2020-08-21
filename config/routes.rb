3594bd70af0ad06a8199c4bffd131205469412f9
yokoto <yo44ko55@gmail.com>
2020-08-21 22:38:46 +0900

    add retirements controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :events
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy', as: :logout
+
+  resource :retirements
 
   resources :events do
     resources :tickets
