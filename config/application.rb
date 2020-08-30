00b03c5821421332db35681d253cd5509ce0e681
yokoto <yo44ko55@gmail.com>
2020-08-30 11:20:18 +0900

    modify events#edit to show thumbnail image

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module AwesomeEvents
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local
+    config.active_storage.variant_processor = :vips
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
