b9a5414a75cc20e3e684cb6c4013e3d2954630fd
yokoto <yo44ko55@gmail.com>
2020-08-31 22:28:56 +0900

    add elasticsearch form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,15 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate
 
   def index
+    @event_search_form = EventSearchForm.new(event_search_form)
     @events = Event.page(params[:page]).per(10)
                    .where('start_at > ?', Time.zone.now)
                    .order(:start_at)
+  end
+
+  private
+
+  def event_search_form
+    params.fetch(:event_search_form, {}).permit(:keyword, :start_at).merge(page: params[:page])
   end
 end
