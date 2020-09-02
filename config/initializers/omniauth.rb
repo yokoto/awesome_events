be4d35ac166095412c7c68bbcbed9875cd5b113f
yokoto <yo44ko55@gmail.com>
2020-09-02 23:54:15 +0900

    modify omniauth environment settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,
-           Rails.application.credentials.github[:client_id],
-           Rails.application.credentials.github[:client_secret]
+  if Rails.env.development? || Rails.env.test?
+    provider :github, 'f1d222104c2027c14d5f', '3065170f38866d00092367578d382423df9a8795'
+  else
+    provider :github,
+             Rails.application.credentials.github[:client_id],
+             Rails.application.credentials.github[:client_secret]
+  end
 end
